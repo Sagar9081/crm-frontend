d5b2d07b04f1f93182984535d19793ac85b2542d
Sagar9081 <sagarch9081@gmail.com>
2021-10-20 12:45:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { Container, Modal, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Entry } from "./page/entry/Entry.page";
+
+import { Dashboard } from "./page/dashboard/Dashboard.page";
+import { AddTicket } from "./page/new-ticket/AddTicket.page";
+import { TicketLists } from "./page/ticket-list/TicketLists.page";
+import { Ticket } from "./page/ticket/Ticket.page";
+import { PrivateRoute } from "./components/private-route/PrivateRoute.comp";
+import { ErrorPage } from "./error";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Entry />
+          </Route>
+          <Route exact path="/error">
+            <ErrorPage />
+          </Route>
+
+          <PrivateRoute exact path="/dashboard">
+            <Dashboard />
+          </PrivateRoute>
+          <PrivateRoute exact path="/add-ticket">
+            <AddTicket />
+          </PrivateRoute>
+          <PrivateRoute exact path="/ticket/:tId">
+            <Ticket />
+          </PrivateRoute>
+          <PrivateRoute exact path="/tickets">
+            <TicketLists />
+          </PrivateRoute>
+
+          <PrivateRoute path="*">
+            <Container>
+              <Modal.Dialog>
+                <Modal.Header>
+                  <Modal.Title>
+                    <h4>Ticket closed successfully!.</h4>
+                  </Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                  <p>
+                    <h6>Thank you for closing the ticket!!</h6>
+                  </p>
+                </Modal.Body>
+
+                <Modal.Footer>
+                  <Link to="/dashboard">
+                    <Button variant="primary">Exit</Button>
+                  </Link>
+                </Modal.Footer>
+              </Modal.Dialog>
+            </Container>
+          </PrivateRoute>
+        </Switch>
+      </Router>
     </div>
   );
 }
